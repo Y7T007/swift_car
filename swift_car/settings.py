5c8fce958522d5b6ff81aa085229a91437a45d63
Y7T007 <yassir7t@gmail.com>
2024-05-18 00:13:50 +0100

    Fixing allowed hosts

--- swift_car/settings.py
+++ swift_car/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-7!zf8hy^+$z#p18f=kz9t_#vj0q2&rykgdcex=ru63*t7gehjn
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "swift-car-django-server-4c51acec5937.herokuappcom",
+    "swift-car-django-server-4c51acec5937.herokuapp.com",
     "localhost",
     "127.0.0.1",
 ]
