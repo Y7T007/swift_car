c93f8e838dc66d9ad38e2f80d03b7804545f2ee6
Y7T007 <yassir7t@gmail.com>
2024-05-18 20:54:37 +0100

    Fixing update manager route

--- managers_app/urls.py
+++ managers_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('view-all', views.view_all_managers, name='view_all_managers'),
     path('view/<str:manager_id>', views.view_manager, name='view_manager'),
     path('add', views.add_manager, name='add_manager'),
-    path('update', views.update_manager, name='update_manager'),
+    path('update/<str:manager_id>', views.update_manager, name='update_manager'),
     path('remove/<int:manager_id>', views.remove_manager, name='remove_manager'),
     path('login', views.login, name='login'),
 ]
