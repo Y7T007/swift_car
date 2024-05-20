3cd1713dbf39aed0cf473a46b2b0a0a8d1538ea2
Y7T007 <yassir7t@gmail.com>
2024-05-20 02:55:04 +0100

    Updating the view car url to take str instead of int

--- cars_app/urls.py
+++ cars_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('add', views.add_car, name='add_car'),
     path('remove/<int:car_id>', views.remove_car, name='remove_car'),
     path('view-all', views.view_all_cars, name='view_all_cars'),
-    path('view/<int:car_id>', views.view_car, name='view_car'),
+    path('view/<str:car_id>', views.view_car, name='view_car'),
     path('update/<int:car_id>', views.update_car, name='update_car'),
 ]
