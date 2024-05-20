a3ee983b89d502ae2b3cfe490403ad9b7bed1bf1
Y7T007 <yassir7t@gmail.com>
2024-05-20 04:43:35 +0100

    Fixing the decimal number eddited

--- reservations_app/views.py
+++ reservations_app/views.py
@@ -220,6 +220,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Reservation
 from bson.objectid import ObjectId
+from decimal import Decimal
 
 
 def accept_reservation(request, id):
@@ -227,6 +228,7 @@ def accept_reservation(request, id):
         try:
             reservation = Reservation.objects.get(_id=ObjectId(id))
             reservation.status = 'accepted'
+            reservation.prix= Decimal(reservation.prix)
             reservation.save()
             return JsonResponse({"message": "Reservation accepted successfully"})
         except Reservation.DoesNotExist:
@@ -240,6 +242,7 @@ def decline_reservation(request, id):
         try:
             reservation = Reservation.objects.get(_id=ObjectId(id))
             reservation.status = 'declined'
+            reservation.prix= Decimal(reservation.prix)
             reservation.save()
             return JsonResponse({"message": "Reservation declined successfully"})
         except Reservation.DoesNotExist:
